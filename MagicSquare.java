30167deb7cb95192a14e679cd22d13dfd2e64d04
aiden-meredith <69974601+aiden-meredith@users.noreply.github.com>
2021-03-03 15:38:28 -0600

    Update MagicSquare.java

--- MagicSquare.java
+++ MagicSquare.java
@@ -25,8 +25,61 @@ public class MagicSquare
      */
     public boolean isMagicSquare()
     {
-        return false;   // complete this method
-    }
+      for(int row=0; row<array.length;row++){
+        if(array[row].length!=array.length)
+          return false;
+      }
+
+    int constant = 0;
+    for (int i = 0; i < array[0].length; i++)
+       constant += array[0][i];
+       
+    for (int[] row : array){
+      int rowSum = 0;
+      for (int j = 0; j < row.length; j ++) {
+         rowSum += row[j]; 
+      }
+      if (rowSum != constant)
+         return false;
+   }
+
+   //check columns 
+  for(int col = 0; col < array[0].length; col++){
+     int colSum = 0;
+      for (int row = 0; row < array.length; row++) {
+         colSum += array[row][col]; 
+      }
+      if (colSum != constant)
+         return false;
+  }
+
+  int sum=0;
+  for(int i=0;i<array.length;i++){
+    sum+=array[i][i];
+
+  }
+  if (sum != constant)
+     return false;
+
+       int sum2=0;
+  int col = array.length-1;
+  for(int i=0;i<array.length;i++){
+    sum2+=array[i][col];
+    col--;
+  }
+  if (sum2 != constant)
+     return false;
+
+
+
+
+
+
+
+
+
+return true;     
+  }
 
     /**
      * Returns a String which represents the array.
